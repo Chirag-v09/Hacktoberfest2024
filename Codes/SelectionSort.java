dd770fcfae7ece41effc8ae58dd931160e018dd8
sachin <sachin.baghel1234@gmail.com>
2024-10-29 01:52:40 +0530

    Added Selection Sort in Java

---
+++ Codes/SelectionSort.java
@@ -0,0 +1,50 @@
+package Codes;
+
+public class SelectionSort {
+
+    static void selectionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+
+            // Assume the current position holds
+            // the minimum element
+            int min_idx = i;
+
+            // Iterate through the unsorted portion
+            // to find the actual minimum
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+
+                    // Update min_idx if a smaller element
+                    // is found
+                    min_idx = j;
+                }
+            }
+
+            // Move minimum element to its
+            // correct position
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+
+    static void printArray(int[] arr){
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] arr = { 64, 25, 12, 22, 11 };
+
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+}
